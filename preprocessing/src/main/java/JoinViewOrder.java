6cbe3e33e0eefa6f4c9b2fb605aa1a25185d96b2
Ching Yi, Chan <chingyichan.tw@gmail.com>
2015-05-17 15:33:05 +0800

    count view for same pid

--- preprocessing/src/main/java/JoinViewOrder.java
+++ preprocessing/src/main/java/JoinViewOrder.java
@@ -12,7 +12,7 @@ public class JoinViewOrder {
     public static void main(String[] args) throws Exception {
 
         // key: erUid,pid, value: num
-        final Map<String, Integer> erUidPidNums = new HashMap<String, Integer>();
+        final Map<String, Integer> erUidPidBuyNums = new HashMap<String, Integer>();
 
         new DataProcessor(FileManager.fileAsReader("order.csv")).process(new CSVProcessor<Map<String, Integer>>() {
 
@@ -22,12 +22,14 @@ public class JoinViewOrder {
                 // pid,ts,ip,price,num,uid,eruid
                 userData.put(csv[6] + csv[0], Integer.valueOf(csv[4]));
             }
-        }, erUidPidNums);
+        }, erUidPidBuyNums);
 
-        new DataProcessor(FileManager.fileAsReader("train_view.csv")).process(new CSVProcessor<Writer>() {
+        // 計算使用者看過同 1 個商品幾次
+        final Map<String, Integer> erUidPidViewCount = new HashMap<String, Integer>();
+        new DataProcessor(FileManager.fileAsReader("train_view.csv")).process(new CSVProcessor<Map<String, Integer>>() {
 
             @Override
-            public void process(String[] csv, Writer writer) throws Exception {
+            public void process(String[] csv, Map<String, Integer> writer) throws Exception {
                 //
                 //                qty:EHC qrtt1$ head view.csv
                 //                pid,ts,ip,uid,eruid
@@ -35,12 +37,33 @@ public class JoinViewOrder {
                 String key = csv[4] + "" + csv[0];
                 ArrayList<String> l = new ArrayList<String>();
                 l.addAll(Arrays.asList(csv));
-                if (erUidPidNums.containsKey(key)) {
+                if (!erUidPidViewCount.containsKey(key)) {
+                    erUidPidViewCount.put(key, 0);
+                }
+                erUidPidViewCount.put(key, erUidPidViewCount.get(key) + 1);
+            }
+        }, erUidPidViewCount);
+
+        new DataProcessor(FileManager.fileAsReader("train_view.csv")).process(new CSVProcessor<Writer>() {
+
+            @Override
+            public void process(String[] csv, Writer writer) throws Exception {
+                String key = csv[4] + "" + csv[0];
+                ArrayList<String> l = new ArrayList<String>();
+                l.addAll(Arrays.asList(csv));
+                if (erUidPidBuyNums.containsKey(key)) {
                     l.add("1"); // 有買
-                    l.add("" + erUidPidNums.get(key)); // 數量 0
+                    l.add("" + erUidPidBuyNums.get(key)); // 數量 0
                 } else {
                     l.add("0"); // 沒買
                     l.add("0"); // 數量 0
+                }
+
+                // view count
+                if (erUidPidViewCount.containsKey(key)) {
+                    l.add("" + erUidPidViewCount.get(key));
+                } else {
+                    l.add("0");
                 }
 
                 writer.write(Joiner.on(",").join(l) + "\n");
