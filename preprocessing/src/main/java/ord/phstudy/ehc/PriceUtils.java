72db74734e5b4d5db560900164244bcd57d688a2
Study <ph.study@gmail.com>
2015-05-21 00:23:46 +0800

    improve PriceUtils

--- preprocessing/src/main/java/ord/phstudy/ehc/PriceUtils.java
+++ preprocessing/src/main/java/ord/phstudy/ehc/PriceUtils.java
@@ -5,9 +5,7 @@ import file.FileManager;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
 import java.util.Map;
-import java.util.Properties;
 
 /**
  * Created by study on 5/20/15.
@@ -18,18 +16,32 @@ public class PriceUtils {
     static {
         try {
             ClassLoader classLoader = PriceUtils.class.getClassLoader();
+
+            // http://iguang.tw/udn/product/<pid>.html
             File file = new File(classLoader.getResource("product.csv").getFile());
             BufferedReader trainBr = FileManager.fileAsReader(file);
             String line;
 
-            while((line = trainBr.readLine())!= null) {
+            while ((line = trainBr.readLine()) != null) {
                 String[] part = line.split(",");
                 String uid = part[0];
                 int price = Integer.parseInt(part[1]);
                 prices.put(uid, price);
             }
-            System.out.println("prices init.");
-        } catch(Exception e){
+            System.out.println("product prices#1 init.");
+
+            // http://www.cheapcheap.com.tw/kw?q=<desc>
+            file = new File(classLoader.getResource("product2.csv").getFile());
+            trainBr = FileManager.fileAsReader(file);
+
+            while ((line = trainBr.readLine()) != null) {
+                String[] part = line.split(",");
+                String uid = part[0];
+                int price = Integer.parseInt(part[1]);
+                prices.put(uid, price);
+            }
+            System.out.println("product prices#2 init.");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
