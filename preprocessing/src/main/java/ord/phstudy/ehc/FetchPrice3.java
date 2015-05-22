6ea97308394779bd76eb1c5a1e870a2ae040fbc1
Study <ph.study@gmail.com>
2015-05-22 09:01:04 +0800

    tmp

--- preprocessing/src/main/java/ord/phstudy/ehc/FetchPrice3.java
+++ preprocessing/src/main/java/ord/phstudy/ehc/FetchPrice3.java
@@ -7,6 +7,8 @@ import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -16,15 +18,16 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Created by study on 5/20/15.
  */
 public class FetchPrice3 {
+    static Set<String> pids = new HashSet<String>();
 
-    static BufferedReader categoryOnlyBr;
+    static BufferedReader sourceFullBr, sourceNonZeroBr, sourceZeroBr, sourceFailBr;
     static BufferedWriter categoryBw, categoryBw2, categoryBw3, categoryBw4;
     static AtomicInteger cnt = new AtomicInteger(0);
     static String bashUrl = "http://pk.emailcash.com.tw/catalog.asp";
 
     public static void main(String[] args) throws Exception {
-        int corePoolSize = 1;
-        int maximumPoolSize = 1;
+        int corePoolSize = 2;
+        int maximumPoolSize = 2;
         long keepAliveTime = 60;
         int capacity = 100;
 
@@ -33,16 +36,29 @@ public class FetchPrice3 {
                 new ThreadPoolExecutor.CallerRunsPolicy());
 
         //String categoryStr = "A_001_001_003_001";
+        sourceFullBr = FileManager.fileAsReader("products2_full.csv");
+        sourceNonZeroBr = FileManager.fileAsReader("products3_price_nonzero.csv");
+        sourceZeroBr = FileManager.fileAsReader("products3_price_zero.csv");
+        sourceFailBr = FileManager.fileAsReader("products3_fail.csv");
 
-        categoryOnlyBr = FileManager.fileAsReader("products2_full.csv");
-        categoryBw = FileManager.fileAsWriter("products3_full.csv");
-        categoryBw2 = FileManager.fileAsWriter("products3_price_nonzero.csv");
-        categoryBw3 = FileManager.fileAsWriter("products3_price_zero.csv");
-        categoryBw4 = FileManager.fileAsWriter("products3_fail.csv");
+
+        categoryBw = FileManager.fileAsWriter("products3_2_full.csv");
+        categoryBw2 = FileManager.fileAsWriter("products3_2_price_nonzero.csv");
+        categoryBw3 = FileManager.fileAsWriter("products3_2_price_zero.csv");
+        categoryBw4 = FileManager.fileAsWriter("products3_2_fail.csv");
 
 
         String line;
-        while ((line = categoryOnlyBr.readLine()) != null) {
+
+        while ((line = sourceNonZeroBr.readLine()) != null) {
+            pids.add(line.split(",")[0]);
+        }
+        while ((line = sourceZeroBr.readLine()) != null) {
+            pids.add(line.split(",")[0]);
+        }
+
+
+        while ((line = sourceFullBr.readLine()) != null) {
             if (cnt.getAndIncrement() % 100 == 0) {
                 categoryBw.flush();
                 categoryBw2.flush();
@@ -72,6 +88,10 @@ public class FetchPrice3 {
             String title = parts[2];
             String price = parts[3];
 
+            if (pids.contains(pid)) { // skip
+                return;
+            }
+
             try {
                 if (!"0".equals(price)) { // skip
                     return;
@@ -98,7 +118,7 @@ public class FetchPrice3 {
             } catch (Exception e) {
                 try {
                     categoryBw4.write(pid + "," + upid + "," + title + "," + price + "\n");
-                } catch(Exception e1) {
+                } catch (Exception e1) {
                     e1.printStackTrace();
                 }
 
