da807ff401664c1563e91b081b04b80b0831ae6f
Ching Yi, Chan <chingyichan.tw@gmail.com>
2015-05-23 01:15:08 +0800

    build model by click-history

---
+++ preprocessing/src/main/java/org/qty/Answer.java
@@ -0,0 +1,25 @@
+package org.qty;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+public class Answer {
+
+    public static Set<String> getPidSet() throws IOException {
+        Set<String> s = new HashSet<String>();
+        for (String line : IOUtils.readLines(Answer.class.getResourceAsStream("/ANSWER"))) {
+            line = StringUtils.substringAfter(line, ",");
+            line = StringUtils.substringBefore(line, " ");
+            s.add(StringUtils.strip(line));
+        }
+        return s;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Answer.getPidSet();
+    }
+}
